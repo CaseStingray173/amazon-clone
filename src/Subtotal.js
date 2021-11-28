7b64bf26b2e435c4ede03671dbc761f08c36321b
Vraj Patel <patel.vraj3@gmail.com>
2021-11-28 09:07:28 -0500

    Added useHistory to Subtotal page. When you click on the "Proceed to Checkout" button it will route you to the payment page using the useHistory

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -4,9 +4,10 @@ import CurrencyFormat from "react-currency-format";
 import ShoppingBasket from '@mui/icons-material/ShoppingBasket';
 import { useStateValue } from "./StateProvider"; 
 import { getBasketTotal } from './reducer';
+import { useHistory } from "react-router-dom";
 
 function Subtotal() {
-
+    const history = useHistory();
     const [{ basket }, dispatch] = useStateValue();
     
     return (
@@ -31,7 +32,7 @@ function Subtotal() {
                 prefix={"$"}
             />
 
-            <button>Proceed to Checkout</button>
+            <button onClick={e => history.push('/payment')}>Proceed to Checkout</button>
         </div>
     );
 }
