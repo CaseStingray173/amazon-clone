1239f31e5873117830adaf17d963521cf2369acb
Vraj Patel <patel.vraj3@gmail.com>
2021-11-28 10:23:57 -0500

    misspelled "className". Added the <Link> to take you back to checkout page. Added <CheckoutProduct> to view the items on payment page

--- src/Payment.js
+++ src/Payment.js
@@ -1,19 +1,27 @@
 import React from 'react'
+import CheckProduct from './CheckProduct';
 import "./Payment.css"
 import { useStateValue } from './StateProvider';
+import { Link } from "react-router-dom";
 
 function Payment() {
     const [{basket, user}, dispatch] = useStateValue();
     
     return (
-        <div classnName="payment">
+        <div className="payment">
 
             <div className="payment_container">
+                <h2>
+                    Checkout (
+                        <Link to="/checkout">{basket?.length} items</Link>
+                    )
+                </h2>
 
                 {/* PAYMENT SECTION -- DELIVERY ADDRESS */}
                 <div className="payment_section">
+
                     <div className="payment_title">
-                        <h3>Delivery Address</h3>
+                        <h3>Delivery Address: </h3>
                     </div>
 
                     <div className="payment_address">
@@ -22,16 +30,42 @@ function Payment() {
                         <p>Los Angeles, CA</p>
                         <p>19222</p>
                     </div>
+
                 </div>
 
                 {/* PAYMENT SECTION -- REVIEW ITEMS */}
                 <div className="payment_section">
-                    
+
+                    <div className="payment_title">
+                        <h3>Review items and delivery address</h3>
+                    </div>
+
+                    <div className="payment_items">
+                        {/* ALL THE PRODUCTS THAT YOU HAVE CHECKED OUT*/}
+                        {basket.map(item => (
+                            <CheckProduct
+                                id={item.id}
+                                title={item.title}
+                                image={item.image}
+                                price={item.price}
+                                rating={item.rating}
+                            />
+                        ))}
+                    </div>  
+
                 </div>
 
                 {/* PAYMENT SECTION -- PAYMENT METHOD */}
                 <div className="payment_section">
-                    
+
+                    <div className="payment_title">
+                        <h3>Payment Method</h3>
+                    </div>
+
+                    <div className="payment_details">
+                        {/* Stripe payment stuff */}
+                    </div>
+
                 </div>
 
             </div>
