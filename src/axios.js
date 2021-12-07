674b0e7ebb614b936557822150d10033bf184945
Vraj Patel <patel.vraj3@gmail.com>
2021-12-07 17:58:27 -0500

    added the baseURL

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "..." // THE AIP {cloud function} URL
+    baseURL: "http://localhost:5001/clone-6533c/us-central1/api" // THE AIP {cloud function} URL
 });
 
 export default instance;
